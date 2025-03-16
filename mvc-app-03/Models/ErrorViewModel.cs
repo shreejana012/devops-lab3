3ccc75640db055acbb7962ccf18040c518c31697
Shrijana Poudel <spoude14@my.centennialcollege.ca>
2025-03-16 15:34:54 -0400

    first commit

---
+++ mvc-app-03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace mvc_app_03.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
